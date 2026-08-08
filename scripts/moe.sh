98022fe0682ff4065f7348a745de2e353c3eabd9
rasel-isu <rasel@iastate.edu>
2026-08-08 18:49:14 -0500

    moe

--- scripts/moe.sh
+++ scripts/moe.sh
@@ -2,11 +2,11 @@ eval $(/lustre/hdd/LAS/qli-lab/rasel/apps/miniconda3/bin/conda shell.bash hook)
 source /lustre/hdd/LAS/qli-lab/rasel/apps/miniconda3/etc/profile.d/conda.sh
 conda activate bio-nlp-next
 HOST="http://127.0.0.1:11435"
-
-
+HOST_ARGS=(--host "$HOST")
 python -m moe.run_moe_eval \
   --model "$MODEL" \
   --setting zero_shot \
   --datasets ncbi_disease \
   --max_instances 5 \
   "${HOST_ARGS[@]}"
+ 
